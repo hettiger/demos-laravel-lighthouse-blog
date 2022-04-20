8625a2bb8b129c889ecc6c28c7c69072a1b530e4
Martin Hettiger <martin@hettiger.com>
2022-04-20 21:52:38 +0200

    Implement loading indicator

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+
+  isLoading = false;
+
+  destroy$ = new Subject();
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(takeUntil(this.destroy$)).subscribe(event => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.isLoading = true;
+          break;
+        }
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationError: {
+          this.isLoading = false;
+          break;
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+}

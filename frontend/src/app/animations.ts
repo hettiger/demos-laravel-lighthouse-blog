94d786f4a500e693c0728970a4a0efc51eeee976
Martin Hettiger <martin@hettiger.com>
2022-04-23 09:44:19 +0200

    Simplify fadeAnimation

--- frontend/src/app/animations.ts
+++ frontend/src/app/animations.ts
@@ -1,28 +1,20 @@
-import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 
 export const fadeAnimation =
   trigger('routeAnimations', [
-    transition('* <=> *', [
+    transition('* => *', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-        })
+        style({ position: 'absolute', top: 0, left: 0, right: 0 })
       ]),
       query(':enter', [
         style({ opacity: 0 })
       ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('300ms ease-out', style({ opacity: 0 }))
-        ]),
-        query(':enter', [
-          animate('300ms 300ms ease-out', style({ opacity: 1 }))
-        ]),
+      query(':leave', [
+        animate('150ms', style({ opacity: 0 }))
+      ], { optional: true }),
+      query(':enter', [
+        animate('150ms', style({ opacity: 1 }))
       ]),
     ]),
   ]);

fbb95b87ea101f9e2a736edff7236da3ae532292
Martin Hettiger <martin@hettiger.com>
2022-04-27 22:36:16 +0200

    Add NavigationHistoryService unit tests

--- frontend/src/app/services/navigation-history.service.spec.ts
+++ frontend/src/app/services/navigation-history.service.spec.ts
@@ -1,21 +1,58 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { NavigationHistoryService } from './navigation-history.service';
+import { Component } from '@angular/core';
+
+@Component({
+  template: ''
+})
+class TestComponent {}
 
 describe('NavigationHistoryService', () => {
   let service: NavigationHistoryService;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'test-a', component: TestComponent },
+          { path: 'test-b', component: TestComponent },
+          { path: 'test-c', component: TestComponent },
+        ]),
       ]
     });
     service = TestBed.inject(NavigationHistoryService);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('history', () => {
+    it('should initialize to an empty array', () => {
+      expect(service.history).toEqual([]);
+    });
+
+    it('should return one URL after a navigation took place', async () => {
+      await router.navigate(['test-a']);
+
+      expect(service.history).toEqual(['/test-a']);
+    });
+
+    it("returns all URL's that we've navigated to", async () => {
+      await router.navigate(['test-a']);
+      await router.navigate(['test-b']);
+      await router.navigate(['test-c']);
+
+      expect(service.history).toEqual([
+        '/test-a',
+        '/test-b',
+        '/test-c',
+      ]);
+    });
   });
 });

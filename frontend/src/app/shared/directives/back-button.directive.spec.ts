f9d9deaf27aa068331885d342966640e1124e6b5
Martin Hettiger <martin@hettiger.com>
2022-04-27 22:23:38 +0200

    Fix back button directive unit tests

--- frontend/src/app/shared/directives/back-button.directive.spec.ts
+++ frontend/src/app/shared/directives/back-button.directive.spec.ts
@@ -1,8 +1,70 @@
 import { BackButtonDirective } from './back-button.directive';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { NavigationHistoryService } from '../../services/navigation-history.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<button appBackButton></button>`,
+})
+class TestComponent { }
 
 describe('BackButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BackButtonDirective();
-    expect(directive).toBeTruthy();
+  let navigationHistoryService: NavigationHistoryService;
+  let location: Location;
+  let router: Router;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        BackButtonDirective,
+      ],
+      imports: [
+        RouterTestingModule,
+      ],
+    }).compileComponents();
+
+    navigationHistoryService = TestBed.inject(NavigationHistoryService);
+    location = TestBed.inject(Location);
+    router = TestBed.inject(Router);
+  });
+
+  const createComponent = () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.directive(BackButtonDirective));
+    return { fixture, button };
+  }
+
+  it('should navigate back using Location.back() when history is available', () => {
+    const { button } = createComponent();
+    const locationSpy = spyOn(location, 'back');
+    spyOnProperty(navigationHistoryService, 'history').and.returnValue([
+      'https://…',
+      'https://…',
+    ]);
+
+    button.nativeElement.click();
+
+    expect(locationSpy).toHaveBeenCalledOnceWith();
+  });
+
+  it('should fall back to Router.navigate() when history is missing', () => {
+    const { button } = createComponent();
+    const routerSpy = spyOn(router, 'navigate');
+    spyOnProperty(navigationHistoryService, 'history').and.returnValue([
+      'https://…',
+    ]);
+
+    button.nativeElement.click();
+
+    expect(routerSpy).toHaveBeenCalledOnceWith(
+      ['..'],
+      { relativeTo: TestBed.inject(ActivatedRoute)},
+    );
   });
 });

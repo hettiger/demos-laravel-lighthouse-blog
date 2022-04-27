7110fa1741f5972f4543199fa0a78fbe236cf326
Martin Hettiger <martin@hettiger.com>
2022-04-27 22:23:50 +0200

    Fix navigation history service unit tests

--- frontend/src/app/services/navigation-history.service.spec.ts
+++ frontend/src/app/services/navigation-history.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavigationHistoryService } from './navigation-history.service';
 
@@ -6,7 +7,11 @@ describe('NavigationHistoryService', () => {
   let service: NavigationHistoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ]
+    });
     service = TestBed.inject(NavigationHistoryService);
   });
 

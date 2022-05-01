befe370f35d33748d50140828b7861d3daffcb73
Martin Hettiger <martin@hettiger.com>
2022-05-01 11:15:06 +0200

    Add missing PostFormComponent cancel button test

--- frontend/src/app/posts/components/post-form/post-form.component.spec.ts
+++ frontend/src/app/posts/components/post-form/post-form.component.spec.ts
@@ -3,15 +3,19 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 import { PostFormComponent } from './post-form.component';
 import { SharedModule } from '../../../shared/shared.module';
-import { of } from 'rxjs';
 import { Post } from '../../entities';
+import { BackButtonDirective } from '../../../shared/directives/back-button.directive';
 
 describe('PostFormComponent', () => {
   let component: PostFormComponent;
   let fixture: ComponentFixture<PostFormComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,14 +29,23 @@ describe('PostFormComponent', () => {
     fixture = TestBed.createComponent(PostFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('cancel', () => {
+    it('navigates back', () => {
+      expect(debugElement.queryAll(By.directive(BackButtonDirective)).filter(
+        (button) => button.nativeElement.textContent === 'Cancel',
+      ).length).toBeGreaterThanOrEqual(1);
+    });
+  });
+
   describe('submit', () => {
-    it('redirects back on success', () => {
+    it('navigates back on success', () => {
       const routerSpy = spyOn(TestBed.inject(Router), 'navigate');
       component.action = () => of({} as Post);
 

5b9f85296f328c00659dc61c9f77fea4c1997c9a
Martin Hettiger <martin@hettiger.com>
2022-05-21 11:30:26 +0200

    Implement basic login form UI

--- resources/angular/src/app/auth/components/login/login.component.ts
+++ resources/angular/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageBag } from '../../../entities';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  serverErrors: MessageBag = {};
+  isLoading = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  submit(credentials: any) {
+    this.isLoading = true;
+
+    try {
+      console.log(credentials);
+    } finally {
+      this.isLoading = false;
+    }
+  }
 }
